17b21772ff4e34c7ffd66ade9866505ddf832665
facundo cordoba <facucor.15@gmail.com>
2020-11-20 14:13:33 -0300

    feature-1: server express raised

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-consol.log("hi")
+const express = require('express');
+const app = express();
+const port = 3000;
+app.use(express.static('data'));
+
+app.listen(port, () => {
+  console.log(`CaldAR app listening at http://localhost:${port}`);
+});
