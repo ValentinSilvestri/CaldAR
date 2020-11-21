754cfae098ef4197664864a88784323cb0a3ff39
facundo cordoba <facucor.15@gmail.com>
2020-11-21 13:32:20 -0300

    feature-1: start built into the package

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-app.use(express.static('data'));
 
 app.listen(port, () => {
   console.log(`CaldAR app listening at http://localhost:${port}`);
